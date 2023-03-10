a1cd03026d9f359f224a7f74ea67fbed3f991760
Laurent Dislaire <siladire@free.fr>
2023-03-10 18:05:57 +0100

    introduction au routing

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,3 +1,14 @@
-export default function FirstPage () {
-  return <h1>Premier Post</h1>
+import Link from 'next/link'
+//import styles from '../../styles/Home.module.css'
+
+
+export default function FirstPage() {
+  return (
+    <>
+      <h1 /*className={styles.title}*/>Premier Post</h1>
+      <Link href="/">
+        <h2>Retour à l'accueil</h2>
+      </Link>
+    </>
+  )
 }
